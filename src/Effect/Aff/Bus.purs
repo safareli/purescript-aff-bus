7983296f5344044afe11ea03b337287c2e4d8020
Irakli Safareli <i.safareli@gmail.com>
2019-09-22 20:04:36 +0400

    add consume

--- src/Effect/Aff/Bus.purs
+++ src/Effect/Aff/Bus.purs
@@ -17,6 +17,9 @@ limitations under the License.
 module Effect.Aff.Bus
   ( make
   , read
+  , consume
+  , consumeLatest
+  , Step(..)
   , write
   , split
   , kill
@@ -33,34 +36,58 @@ module Effect.Aff.Bus
 import Prelude
 
 import Control.Lazy (fix)
+import Control.Monad.Reader (ReaderT, ask, lift, runReaderT)
 import Control.Monad.Rec.Class (forever)
 import Data.Either (Either(..))
-import Data.Foldable (foldl, sequence_, traverse_)
-import Data.List (List(..), (:))
+import Data.Foldable (for_)
+import Data.List (List, catMaybes, (:))
+import Data.Maybe (Maybe(..))
+import Data.Traversable (for)
 import Data.Tuple (Tuple(..))
+import Effect (Effect)
 import Effect.AVar as EffAVar
-import Effect.Aff (Aff, Error, launchAff_, try)
+import Effect.Aff (Aff, Error, generalBracket, killFiber, launchAff, launchAff_, try)
 import Effect.Aff.AVar (AVar)
 import Effect.Aff.AVar as AVar
+import Effect.Aff.CleanUp as CU
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Exception as Exn
+import Effect.Ref (Ref)
+import Effect.Ref as Ref
+import Unsafe.Coerce (unsafeCoerce)
 
 data Cap
 
-data Bus (r ∷ # Type) a = Bus (AVar (Either Error a)) (AVar (List (AVar a)))
+data Bus (r :: # Type) a = Bus (AVar (Either Error a)) (AVar (List (Ref (Maybe (Consumer a)))))
+
+
+
+foreign import data Consumer :: Type -> Type
+
+type ConsumerR output input =
+  { outputVar :: AVar output
+  , consumer :: input -> ReaderT (AVar output) Effect Unit
+  }
+
+mkConsumer :: ∀ output input. ConsumerR output input -> Consumer input
+mkConsumer = unsafeCoerce
+
+runConsumer :: ∀ input r. (∀ output. ConsumerR output input -> r) -> Consumer input -> r
+runConsumer = unsafeCoerce
+
 
 type BusR = BusR' ()
 
-type BusR' r = Bus (read ∷ Cap | r)
+type BusR' r = Bus (read :: Cap | r)
 
 type BusW = BusW' ()
 
-type BusW' r = Bus (write ∷ Cap | r)
+type BusW' r = Bus (write :: Cap | r)
 
-type BusRW = Bus (read ∷ Cap, write ∷ Cap)
+type BusRW = Bus (read :: Cap, write :: Cap)
 
 -- | Creates a new bidirectional Bus which can be read from and written to.
-make ∷ ∀ m a. MonadEffect m ⇒ m (BusRW a)
+make :: ∀ m a. MonadEffect m ⇒ m (BusRW a)
 make = liftEffect do
   cell ← EffAVar.empty
   consumers ← EffAVar.new mempty
@@ -71,39 +98,100 @@ make = liftEffect do
     resE ← AVar.read cell
     case resE of
       Left err -> do
-        vars ← AVar.take consumers
+        pure unit
+        cs ← AVar.take consumers
         liftEffect do
-          traverse_ (EffAVar.kill err) vars
+          for_ cs $ \cRef -> do
+            mbC <- liftEffect $ Ref.read cRef
+            for_ mbC (runConsumer (_.outputVar >>> EffAVar.kill err))
           EffAVar.kill err consumers
           EffAVar.kill err cell
       Right res -> do
         void $ AVar.take cell
-        vars ← AVar.take consumers
-        AVar.put Nil consumers
-        sequence_ (foldl (\xs a → AVar.put res a : xs) mempty vars)
+        cs ← AVar.take consumers
+        consumersMb <- for cs $ \cRef -> do
+          mbC <- liftEffect $ Ref.read cRef
+          case mbC of
+            Nothing -> pure Nothing
+            Just c -> c # runConsumer \{outputVar, consumer} -> do
+              try (liftEffect (runReaderT (consumer res) outputVar)) >>= case _ of
+                Right _ -> do
+                  pure $ Just cRef
+                Left err -> do
+                  AVar.kill err outputVar
+                  pure $ Nothing
+        AVar.put (catMaybes consumersMb) consumers
         loop
   pure $ Bus cell consumers
 
 -- | Blocks until a new value is pushed to the Bus, returning the value.
-read ∷ ∀ a r. BusR' r a → Aff a
-read (Bus _ consumers) = do
-  res' ← AVar.empty
-  cs ← AVar.take consumers
-  AVar.put (res' : cs) consumers
-  AVar.take res'
+read :: ∀ a r. BusR' r a -> Aff a
+read bus = consume bus $ \a -> ask >>= AVar.put a >>> launchAff_ >>> lift
 
--- | Pushes a new value to the Bus, yieldig immediately.
-write ∷ ∀ a r. a → BusW' r a → Aff Unit
+data Step res = Loop | Done res
+
+-- | Registers a new consumer on the bus. Blocks until the consumer returns
+-- | `Done x` and then computation is resolved with the `x`. 
+-- | if Effect raises an exception it will be propagated
+consume :: ∀ a r output. BusR' r a -> (a -> ReaderT (AVar output) Effect Unit) -> Aff output
+consume (Bus _ consumers) consumer = CU.runFinallyM do
+  cs ← EffAVar.take consumers # CU.hook do
+    -- If we were able to `take` we must put it back.
+    CU.onResult \cs -> lift $ AVar.put cs consumers
+
+  {outputVar, cRef} <- lift $ liftEffect do
+    outputVar <- EffAVar.empty
+    cRef <- Ref.new $ Just $ mkConsumer {outputVar, consumer}
+    void $ EffAVar.read outputVar \_ -> do
+      -- after outputVar is killed or filled we clear
+      -- the ref to avoid any possible memory leak.
+      Ref.write Nothing cRef
+    pure {outputVar, cRef}
+      
+
+  EffAVar.put (cRef : cs) consumers # CU.hook do
+    -- If we were able to `put` then we must kill outputVar
+    CU.onResult \_ -> (CU.onError <> CU.onFinalError) \err -> do
+      lift $ AVar.kill err outputVar
+    -- Otherwise we should `put` back to `consumers`.
+    CU.onError \_ -> lift $ AVar.put cs consumers
+
+  EffAVar.take outputVar # CU.hook do
+    -- here in any case we kill `outputVar`
+    lift $ AVar.kill (Exn.error "cleanup") outputVar
+
+-- | Same as `consume` but consuming function returns Aff instead of Effect.
+-- | When consuming function is not completed and new value comes in, it gets
+-- | killed and is invoked with new value.
+consumeLatest :: ∀ a r output. BusR' r a -> (a -> Aff (Step output)) -> Aff output
+consumeLatest bus consumer = do
+  fiberRef <- liftEffect $ Ref.new (pure unit)
+  consume bus \val -> do
+    outputVar <- ask
+    lift $ Ref.read fiberRef >>= (killFiber (Exn.error "kill from consumeLatest") >>> launchAff_)
+    fiber <- lift $ launchAff do
+      void $ generalBracket (pure unit)
+        { killed: const pure
+        , failed: \err _ -> AVar.kill err outputVar
+        , completed: \cs _ -> case cs of
+            Loop -> pure unit
+            Done res -> AVar.put res outputVar
+        }
+        (const $ consumer val)
+    lift $ Ref.write fiber fiberRef
+
+-- | Pushes a new value to the Bus, yielding immediately.
+write :: ∀ a r. a -> BusW' r a -> Aff Unit
 write a (Bus cell _) = AVar.put (Right a) cell
 
 -- | Splits a bidirectional Bus into separate read and write Buses.
-split ∷ ∀ a. BusRW a → Tuple (BusR a) (BusW a)
+split :: ∀ a. BusRW a -> Tuple (BusR a) (BusW a)
 split (Bus a b) = Tuple (Bus a b) (Bus a b)
 
 -- | Kills the Bus and propagates the exception to all pending and future consumers.
 -- | `kill` is idempotent and blocks until killing process is fully finishes, i.e.
 -- | `kill err bus *> isKilled bus` will result with `true`.
-kill ∷ ∀ a r. Exn.Error → BusW' r a → Aff Unit
+kill :: ∀ a r. Exn.Error -> BusW' r a -> Aff Unit
 kill err bus@(Bus cell consumers) = do
   unlessM (isKilled bus) do
     -- If there are multiple parallel processes executing `kill` at the same time,
@@ -115,5 +203,5 @@ kill err bus@(Bus cell consumers) = do
     void $ try $ forever $ AVar.read cell
 
 -- | Synchronously checks whether a Bus has been killed.
-isKilled ∷ ∀ m a r. MonadEffect m ⇒ Bus r a → m Boolean
+isKilled :: ∀ m a r. MonadEffect m ⇒ Bus r a -> m Boolean
 isKilled (Bus cell _) = liftEffect $ EffAVar.isKilled <$> EffAVar.status cell
--- test/Main.purs
+++ test/Main.purs
@@ -18,53 +18,205 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Error.Class (throwError)
-import Control.Parallel (parSequence_)
+import Control.Alt (alt)
+import Control.Monad.Reader (ask)
+import Control.Monad.Trans.Class (lift)
+import Control.Parallel (parSequence_, parallel, sequential)
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..), either)
 import Effect (Effect)
-import Effect.Aff (Aff, Milliseconds(..), attempt, delay, forkAff, joinFiber, runAff_)
+import Effect.Aff (Aff, Milliseconds(..), delay, forkAff, joinFiber, launchAff_, never, runAff_, throwError, try)
+import Effect.Aff.AVar as AVar
 import Effect.Aff.Bus as Bus
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Effect.Exception (error, throwException)
+import Effect.Exception (error)
 import Effect.Ref as Ref
 import Test.Assert (assertEqual', assertTrue')
+import Unsafe.Coerce (unsafeCoerce)
 
-test_readWrite ∷ Bus.BusRW Int -> Aff Unit
+timeout :: forall a. Milliseconds -> Aff a -> Aff a
+timeout ms aff = sequential $ parallel aff `alt` parallel delayAndKill
+  where delayAndKill = delay ms *> throwError (error $ "Timeout after " <> show ms)
+
+suit :: String -> Aff Unit -> Aff Unit
+suit name aff = do
+  liftEffect $ log ("[Start] " <> name)
+  try aff >>= case _ of
+    Left err → do
+      liftEffect $ log ("[Error] " <> name)
+      throwError err
+    Right res -> do
+      liftEffect $ log ("[Done]")
+      pure res
+
+test_isKilled :: Aff Unit
+test_isKilled = do
+  bus <- Bus.make
+  let err = error "Done"
+
+  Bus.kill err bus
+
+  isKilled <- Bus.isKilled bus
+  liftEffect $ assertTrue' "`isKilled` immediately after `kill` results `true`" isKilled
+
+test_kill_parallel :: Aff Unit
+test_kill_parallel = do
+  bus <- Bus.make
+
+  let err = error "Done"
+
+  parSequence_
+    [ Bus.kill err bus
+    , Bus.kill err bus
+    , Bus.kill err bus
+    ]
+
+  isKilled <- Bus.isKilled bus
+  liftEffect $ assertTrue' "killing in parallel must be safe" isKilled
+
+test_kill_idempotent :: Aff Unit
+test_kill_idempotent = do
+  bus <- Bus.make
+
+  let err = error "Done"
+
+  Bus.kill err bus
+  Bus.kill err bus
+  Bus.kill err bus
+  Bus.kill err bus
+
+  isKilled <- Bus.isKilled bus
+  liftEffect $ assertTrue' "killing must be idempotent" isKilled
+
+
+
+test_kill_read :: Aff Unit
+test_kill_read = do
+  bus <- Bus.make
+
+  readFiber <- forkAff $ Bus.read bus
+
+  let err = error "Done"
+
+  Bus.kill err bus
+
+  isKilled <- Bus.isKilled bus
+  liftEffect $ assertTrue' "`isKilled` immediately after `kill` results `true`" isKilled
+
+  readFiberRes <- try (joinFiber readFiber)
+  liftEffect $ assertEqual' "`read` from bus which is killed raises same error which was used to `kill`"
+    {actual: either show absurd readFiberRes, expected: show err}
+
+  readRes <- try (Bus.read bus)
+  liftEffect $ assertEqual' "`read` from killed bus should resolve with same error which was used to `kill`"
+    {actual: lmap show readRes, expected: Left $ show err}
+
+test_consume :: Aff Unit
+test_consume = do
+  bus <- Bus.make
+  ref ← liftEffect $ Ref.new []
+  f1 ← forkAff do
+    res <- try $ Bus.consume bus \res ->
+      lift $ liftEffect $ void $ Ref.modify (_ <> [Right res]) ref
+    void $ liftEffect $ Ref.modify (_ <> [either Left absurd res]) ref
+
+  
+  Bus.write 1 bus
+  Bus.write 2 bus
+  
+  delay $ Milliseconds 20.0
+  Bus.write 3 bus
+
+  let err = error "Done"
+
+  Bus.kill err bus
+
+  joinFiber f1
+
+  res <- liftEffect $ Ref.read ref
+  liftEffect $ assertEqual' "`res` should be as expected"
+    {actual: lmap show <$> res, expected: [Right 1, Right 2, Right 3, Left $ show err]}
+
+
+test_consume_result :: Aff Unit
+test_consume_result = do
+  bus <- Bus.make
+  ref ← liftEffect $ Ref.new []
+  f1 ← forkAff $ Bus.consume bus \res -> ask >>= \avar -> lift $ launchAff_ $ AVar.put res avar
+
+  
+  Bus.write 7 bus
+  Bus.write 9 bus
+
+  fibRes <- joinFiber f1
+
+  liftEffect $ assertEqual' "`consume` should return result"
+    {actual: fibRes, expected: 7}
+
+
+test_consumeLatest_result :: Aff Unit
+test_consumeLatest_result = do
+  bus <- Bus.make
+  ref ← liftEffect $ Ref.new []
+  f1 ← forkAff $ Bus.consumeLatest bus \res -> do
+    delay $ Milliseconds 10.0
+    pure $ Bus.Done res
+
+  
+  Bus.write 7 bus
+  Bus.write 9 bus
+
+  fibRes <- joinFiber f1
+
+  liftEffect $ assertEqual' "`consumeLatest` should return result"
+    {actual: fibRes, expected: 9}
+
+test_consumeLatest :: Aff Unit
+test_consumeLatest = do
+  bus <- Bus.make
+  ref ← liftEffect $ Ref.new []
+  f1 ← forkAff do
+    res <- try $ Bus.consumeLatest bus \res -> do
+      delay $ Milliseconds 10.0
+      void $ liftEffect $ Ref.modify (_ <> [Right res]) ref
+      pure Bus.Loop
+    void $ liftEffect $ Ref.modify (_ <> [either Left absurd res]) ref
+
+  
+  Bus.write 1 bus
+  Bus.write 2 bus
+  delay $ Milliseconds 20.0
+  Bus.write 3 bus
+
+  let err = error "Done"
+  Bus.kill err bus
+
+  joinFiber f1
+
+  res <- liftEffect $ Ref.read ref
+  liftEffect $ assertEqual' "`res` should be as expected"
+    {actual: lmap show <$> res, expected: [Right 2, Left $ show err]}
+
+test_readWrite :: Bus.BusRW Int -> Aff Unit
 test_readWrite bus = do
   ref ← liftEffect $ Ref.new []
-
   let
     proc = do
-      res ← attempt (Bus.read bus)
+      res ← try (Bus.read bus)
       void $ liftEffect $ Ref.modify (_ <> [res]) ref
       either (const $ pure unit) (const proc) res
-
   f1 ← forkAff proc
   f2 ← forkAff proc
+
 
   Bus.write 1 bus
   Bus.write 2 bus
   Bus.write 3 bus
 
   let err = error "Done"
-  -- killing in parallel must be safe
-  parSequence_
-    [ Bus.kill err bus
-    , Bus.kill err bus
-    , Bus.kill err bus
-    ]
-  isKilled <- Bus.isKilled bus
-  liftEffect $ assertTrue' "`isKilled` immediately after `kill` results `true`" isKilled
-  
-  -- kill is idempotent
   Bus.kill err bus
-
-  readRes <- attempt (Bus.read bus)
-  liftEffect $ assertEqual' "`read` from killed bus should resolve with same error which was used to `kill`"
-    {actual: lmap show readRes, expected: Left $ show err}
-
+  
   joinFiber f1
   joinFiber f2
 
@@ -73,22 +225,15 @@ test_readWrite bus = do
     {actual: lmap show <$> res, expected: [Right 1, Right 1, Right 2, Right 2, Right 3, Right 3, Left $ show err, Left $ show err]}
 
 
-main ∷ Effect Unit
-main = do
-  log "Testing read/write/kill..."
-  runTest $ Bus.make >>= test_readWrite
-  runTest $ (liftEffect Bus.make) >>= test_readWrite
-  where
-  runTest t = do
-    isFinishedRef <- Ref.new false
-    runAff_ (isOk isFinishedRef) t
-    runAff_ (either throwException pure) do
-      delay (Milliseconds 100.0)
-      isFinished <- liftEffect $ Ref.read isFinishedRef
-      unless isFinished $ throwError (error "Timeout")
-    where
-    isOk isFinishedRef = case _ of
-      Left err -> throwException err
-      Right res -> do
-        log "ok"
-        Ref.write true isFinishedRef
+main :: Effect Unit
+main = launchAff_ do
+  let timeout' = timeout (Milliseconds 100.0)
+  suit "isKilled" $ timeout' test_isKilled
+  suit "kill in parallel" $ timeout' test_kill_parallel
+  suit "kill is idempotent" $ timeout' test_kill_idempotent
+  suit "kill and read" $ timeout' test_kill_read
+  suit "consume works" $ timeout' test_consume
+  suit "consume can return result" $ timeout' test_consume_result
+  suit "consumeLatest works" $ timeout' test_consumeLatest
+  suit "consumeLatest can return result" $ timeout' test_consumeLatest_result
+  suit "Testing read/write/kill" $ timeout' $ (liftEffect Bus.make) >>= test_readWrite
